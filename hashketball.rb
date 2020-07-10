39fb7bd7cc5f0b0303a8030403162f97d2be9e5a
Israel Canessa <israellcanessa@gmail.com>
2020-07-10 10:58:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -149,7 +149,7 @@ def shoe_size(player_name)
   game_hash.each do |team, team_info|
     team_info.each do |team_level, data|
       if data.class == Array
-        binding.pry
+        #binding.pry
         if data[0].class == Hash
           data.each do |key, value|
             if key[:player_name] == player_name
