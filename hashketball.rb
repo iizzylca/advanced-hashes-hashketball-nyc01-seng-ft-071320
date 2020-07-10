fc338b4446d82dbd3263032e881f04daa3bc6824
Israel Canessa <israellcanessa@gmail.com>
2020-07-10 09:28:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -131,8 +131,7 @@ def num_points_scored(player_name)
   game_hash
   game_hash.each do |team, team_info|
     team_info.each do |team_level_keys, team_stats|
-      binding.pry
-      team_stats.each do |stat_details|
+    
       end
     end
   end
