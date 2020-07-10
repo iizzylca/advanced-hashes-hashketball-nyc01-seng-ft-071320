ada1d0350cf7c73bbdb48fa144efbb08f880060d
Israel Canessa <israellcanessa@gmail.com>
2020-07-10 10:16:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -167,8 +167,7 @@ def team_colors(team_name)
   game_hash.each do |team, team_info|
     team_info.each do |team_level, data|
       if data.class == Array && team_info == team_name
-
-        #binding.pry
+        binding.pry
       end
     end
   end
